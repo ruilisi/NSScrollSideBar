e79335a77a13513dc326ee51f8feab7ab00e9fa4
徐恒一 <920301419@qq.com>
2019-03-13 16:45:12 +0800

    init NSScrollSideBar

---
+++ NSScrollSideBar/NSScrollSideBar.h
@@ -0,0 +1,19 @@
+//
+//  NSScrollSideBar.h
+//  NSScrollSideBar
+//
+//  Created by 徐恒一 on 2019/3/13.
+//  Copyright © 2019 徐恒一. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSScrollSideBar.
+FOUNDATION_EXPORT double NSScrollSideBarVersionNumber;
+
+//! Project version string for NSScrollSideBar.
+FOUNDATION_EXPORT const unsigned char NSScrollSideBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSScrollSideBar/PublicHeader.h>
+
+
